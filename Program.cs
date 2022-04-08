ca48e5be0be73cbfa764fe309cd3e4f4d1b19499
Jakub Kołodziej <94216113+JakubKolodziej21@users.noreply.github.com>
2022-04-08 21:04:11 +0200

    Version 1.2

--- Program.cs
+++ Program.cs
@@ -8,40 +8,47 @@ namespace _4th_Degree__equation
         {
 
 
-        //Przyjmowanie wartości
+            //Przyjmowanie wartości
+            /* TESTY
+            Entering_Values:
+                Console.WriteLine("Dana jest funkcja sześcienna");
+                Console.Write("Podaj a:  ");
+                string A = Console.ReadLine();
 
-        Entering_Values:
-            Console.WriteLine("Dana jest funkcja sześcienna");
-            Console.Write("Podaj a:  ");
-            string A = Console.ReadLine();
+                while (A == "0")
+                { //Sprawdzenie wartości A, która musi być różna od zera
+                    Console.WriteLine("Argument A nie może przyjmować wartości równej zero, podaj inny argument");
+                    goto Entering_Values;
 
-            while (A == "0")
-            { //Sprawdzenie wartości A, która musi być różna od zera
-                Console.WriteLine("Argument A nie może przyjmować wartości równej zero, podaj inny argument");
-                goto Entering_Values;
+                }
 
-            }
+                Console.Write("Podaj b:  ");
+                string B = Console.ReadLine();
 
-            Console.Write("Podaj b:  ");
-            string B = Console.ReadLine();
+                Console.Write("Podaj c:  ");
+                string C = Console.ReadLine();
 
-            Console.Write("Podaj c:  ");
-            string C = Console.ReadLine();
+                Console.Write("Podaj d:  ");
+                string D = Console.ReadLine();
 
-            Console.Write("Podaj d:  ");
-            string D = Console.ReadLine();
-
-            Console.Write("Podaj e:  ");
-            string E = Console.ReadLine();
+                Console.Write("Podaj e:  ");
+                string E = Console.ReadLine();
 
 
-            //Konwertujemy zmienną tekstową na zmienno-przecinkową
+                //Konwertujemy zmienną tekstową na zmienno-przecinkową
 
-            double a = Convert.ToDouble(A);
-            double b = Convert.ToDouble(B);
-            double c = Convert.ToDouble(C);
-            double d = Convert.ToDouble(D);
-            double e = Convert.ToDouble(E);
+                double a = Convert.ToDouble(A);
+                double b = Convert.ToDouble(B);
+                double c = Convert.ToDouble(C);
+                double d = Convert.ToDouble(D);
+                double e = Convert.ToDouble(E);*/
+
+            double a = 3;
+            double b = 6;
+            double c = -123;
+            double d = -126;
+            double e = 1080;
+
 
             //Obliczamy F,G,H
 
@@ -66,23 +73,24 @@ namespace _4th_Degree__equation
             //Obliczamy deltę i porównujemy ją z zerem
 
             double delta = (Math.Pow(q, 2) / 4) + (Math.Pow(p, 3) / 27);
+            Console.WriteLine("Delta = "+delta);
 
             if (delta > 0)
             {
-                DeltaWieksza(w, p, q, delta);
+                DeltaWieksza(w, p, q, delta,g,a,b);
             }
             else if (delta < 0)
             {
-                DeltaMniejsza(w, p, q, delta);
+                DeltaMniejsza(w, p, q, delta,g,a,b);
             }
             else if (delta == 0)
             {
-                DeltaRowna(w, p, q, delta);
+                DeltaRowna(w, p, q, delta,g,a,b);
             }
 
         }
 
-        static void DeltaWieksza(double w, double p, double q, double delta)
+        static void DeltaWieksza(double w, double p, double q, double delta, double g,double a,double b)
         {
             double u = Math.Cbrt((-q / 2.0) + Math.Sqrt(delta));
             double v = Math.Cbrt((-q / 2.0) - Math.Sqrt(delta));
@@ -103,7 +111,7 @@ namespace _4th_Degree__equation
 
         }
 
-        static void DeltaRowna(double w, double p, double q, double delta)
+        static void DeltaRowna(double w, double p, double q, double delta, double g, double a,double b)
         {
             double x1 = w - 2 * Math.Cbrt(q / 2);
             double x23 = w + Math.Cbrt(q / 2);
@@ -111,20 +119,69 @@ namespace _4th_Degree__equation
             Console.WriteLine("Wynikiem równania są dwa pierwiastki rzeczywiste: " + x1 + " oraz drugi-podwójny: " + x23);
         }
 
-        static void DeltaMniejsza(double w, double p, double q, double delta)
+        static void DeltaMniejsza(double w, double p, double q, double delta, double g, double a,double b)
         {
             Console.WriteLine("Trzy pierwiastki rzeczyswiste.");
 
             double fi = Math.Acos((3 * q) / (2 * p * Math.Sqrt(-p / 3.0)));
-            Console.WriteLine($"fi= {fi}");
-
+            
             double x1 = (w + (2 * Math.Sqrt(-p / 3.0)) * Math.Cos(fi / 3.0));
             double x2 = (w + 2 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0 + (2.0 * Math.PI) / 3.0));
             double x3 = (w + 2 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0 + (4.0 * Math.PI) / 3.0));
-            Console.WriteLine("Trzy pierwiastki rzeczywiste:");
-            Console.WriteLine("x1 = " + x1);
-            Console.WriteLine("x2 = " + x2);
-            Console.WriteLine("x3 = " + x3);
+          
+            //Ciąg dalszy
+
+            if(x1 == 0)
+            {
+                Double y1 = x2;
+                Double y2 = x3;
+                double P, Q, R, S;
+                P = Math.Sqrt(y1);
+                Q = Math.Sqrt(y2);
+                R = -(g / (8 * P * Q));
+                S = b / (4 * a);
+                Console.WriteLine("x1 = " + (P + Q) + " + " + (R - S) + "i");
+                Console.WriteLine("x2 = " + (P - Q) + " + " + (-R - S) + "i");
+                Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R - S) + "i");
+                Console.WriteLine("x4 = " + (-P - Q) + " + " + (R - S) + "i");
+
+
+            }
+            else if(x2 == 0)
+            {
+                Double y1 = x1;
+                Double y2 = x3;
+                double P, Q, R, S;
+                P = Math.Sqrt(y1);
+                Q = Math.Sqrt(y2);
+                R = -(g / (8 * P * Q));
+                S = b / (4 * a);
+                Console.WriteLine("x1 = " + (P + Q) + " + " + (R - S) + "i");
+                Console.WriteLine("x2 = " + (P - Q) + " + " + (-R - S) + "i");
+                Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R - S) + "i");
+                Console.WriteLine("x4 = " + (-P - Q) + " + " + (R - S) + "i");
+
+            }
+            else
+            {
+                Double y1 = x1;
+                Double y2 = x2;
+                double P, Q, R, S;
+                P = Math.Sqrt(y1);
+                Q = Math.Sqrt(y2);
+                R = -(g / (8 * P * Q));
+                S = b / (4 * a);
+                Console.WriteLine("x1 = " + (P + Q)  + " + " + (R-S)  + "i");
+                Console.WriteLine("x2 = " + (P - Q)  + " + " + (-R-S) + "i");
+                Console.WriteLine("x3 = " + (-P + Q) + " + " + (-R-S) + "i");
+                Console.WriteLine("x4 = " + (-P - Q) + " + " + (R-S)  + "i");
+
+
+
+            }
+
+
+            
 
 
 
